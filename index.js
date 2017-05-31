157322cb7aa31108ab19b672bb21c5789b52668a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-05-31 10:17:49 +0100

    Add gruntfile, add bourbon

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-app = express();
+const app = express();
+const path = require('path');
 
 app.set("view engine", "ejs");
-
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
